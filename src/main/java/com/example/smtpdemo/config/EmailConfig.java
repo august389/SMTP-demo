376c1307e5e428c5304a65e6c2b40a0b849376aa
Tuan. Nguyen Quoc - CMC Global DU3.11 <nqtuan3@cmcglobal.vn>
2022-06-08 15:39:18 +0700

    config env for bean emailConfig

--- src/main/java/com/example/smtpdemo/config/EmailConfig.java
+++ src/main/java/com/example/smtpdemo/config/EmailConfig.java
@@ -1,6 +1,8 @@
 package com.example.smtpdemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
@@ -9,20 +11,27 @@ import java.util.Properties;
 @Configuration
 public class EmailConfig {
 
+    private Environment env;
+
+    @Autowired
+    public void setEnv(Environment env) {
+        this.env = env;
+    }
+
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
+        mailSender.setHost(env.getProperty("spring.mail.host"));
+        mailSender.setPort(Integer.valueOf(env.getProperty("spring.mail.port")));
 
-        mailSender.setUsername("theophan.cmcglobal@gmail.com");
-        mailSender.setPassword("pzsbtssfwlosqcfo");
+        mailSender.setUsername(env.getProperty("spring.mail.username"));
+        mailSender.setPassword(env.getProperty("spring.mail.password"));
 
         Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
+        props.put("mail.transport.protocol", env.getProperty("spring.mail.properties.transport.protocol"));
+        props.put("mail.smtp.auth", env.getProperty("spring.mail.properties.mail.smtp.auth"));
+        props.put("mail.smtp.starttls.enable", env.getProperty("spring.mail.properties.mail.smtp.starttls.enable"));
+        props.put("mail.debug", env.getProperty("spring.mail.properties.mail.debug"));
 
         return mailSender;
     }
--- src/main/java/com/example/smtpdemo/controller/EmailController.java
+++ src/main/java/com/example/smtpdemo/controller/EmailController.java
@@ -3,14 +3,23 @@ package com.example.smtpdemo.controller;
 import com.example.smtpdemo.service.EmailServiceImpl;
 import com.example.smtpdemo.modal.SimpleMail;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
 @RestController
 public class EmailController {
 
+    @Autowired
+    public JavaMailSender emailSender;
     private EmailServiceImpl emailService;
 
     @Autowired
